74efff7dfa7fc2ad08a45b749eb072d318854af9
Yuval <yuvalvarshevsky@gmail.com>
2022-01-25 21:39:22 +0200

    added logger

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,10 @@
 import express, { Application, Request, Response } from 'express';
+import morgan from 'morgan';
 
 const app: Application = express();
 
 app.use(express.json());
+app.use(morgan('dev'));
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello');
