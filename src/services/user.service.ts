f63c8c567d42438c6bacb2a13742ee353ec735ba
Yuval <yuvalvarshevsky@gmail.com>
2022-03-03 13:19:07 +0200

    removed unnecessary await

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -5,7 +5,7 @@ import { logger } from '../utils/logger';
 
 export async function createUser(input: User) {
   try {
-    return await userModel.create<User>(input);
+    return userModel.create<User>(input);
   } catch (e: any) {
     logger.error('Error when creating user');
     throw new Error(e);
